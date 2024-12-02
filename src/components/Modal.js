d42942e1e74f747c1077fe4bda857a5e12b748f2
Jamil-4khtar <jamil4khtar@gmail.com>
2024-12-02 23:57:51 +0530

    update p

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,7 +9,7 @@ function Modal({ show, onClose }) {
         <div className='model-overlay' onClick={onClose}>
             <button className='model-close' onClick={onClose}>Close</button>
             <div className='model-content'>
-                <p className='model p'>This is the content of the modal.</p>
+                <p className='model-p'>This is the content of the modal.</p>
             </div>
         </div>
     )
